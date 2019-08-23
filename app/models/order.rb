b5b45b12f3e3cc2be59b50354d56a3fb3834147a
Julian Kohlman <juliankohlman@gmail.com>
2019-08-22 20:02:19 -0400

    adds tk file to gitignore

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,15 @@
 class Order < ApplicationRecord
-  belongs_to :customer
-  has_many :order_products
-  has_many :products, through: :order_products
+	belongs_to :customer
+	has_many :order_products
+	has_many :products, through: :order_products
 
+	validates :status, inclusion: { in: %w[pending shipped] }
 
-  validates :status, inclusion: { in: ['pending', 'shipped'] }
+	def shippable?
+		status != 'shipped' && products.count >= 1
+	end
 
-
-  def shippable?
-    status != "shipped" && products.count >= 1
-  end
-
-  def ship
-    shippable? && update(status: "shipped")
-  end
-
+	def ship
+		shippable? && update(status: 'shipped')
+	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,17 @@
 class Product < ApplicationRecord
-  has_many :order_products
-  has_many :orders, through: :order_products
+	has_many :order_products
+	has_many :orders, through: :order_products
 
-  validates :name, presence: true
-  validates :cost_cents, numericality: { only_integer: true, greater_than: 0 }
-  validates :inventory, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	validates :name, presence: true
+	validates :cost_cents, numericality: { only_integer: true, greater_than: 0 }
+	validates :inventory,
+	          numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def available?
-    inventory > 0
-  end
+	def available?
+		inventory.positive?
+	end
 
-  def reduce_inventory
-    update(inventory: inventory - 1)
-  end
+	def reduce_inventory
+		update(inventory: inventory - 1)
+	end
 end
